3bd45f96322ce0dbe1e4196397d35b9095645587
El Cucuy <boogeyman2727@protonmail.com>
2022-01-14 00:27:31 +0300

    Version 1.1 small optimization + bug fixes

--- handlers/users/weather_auto_sender.py
+++ handlers/users/weather_auto_sender.py
@@ -1,7 +1,9 @@
 import asyncio
 import codecs
 import datetime
+import sqlite3
 
+import aiogram.utils.exceptions
 import emoji
 
 from data.config import SERVER_TIMEZONE
@@ -10,8 +12,12 @@ from .get_weather import get_weather
 
 
 async def weather_auto_sender():
+    all_users = tuple()
     while True:
-        all_users = db.select_all_users(subscription=1)
+        try:
+            all_users = db.select_all_users(subscription=1)
+        except sqlite3.OperationalError:
+            print("Creating table Users...")
         for i in all_users:
             time_date = datetime.datetime.now()
             user_timezone = i[5]
@@ -22,10 +28,13 @@ async def weather_auto_sender():
                 time_date -= datetime.timedelta(hours=SERVER_TIMEZONE - user_timezone)
             if time_date.hour == 7:
                 current_weather = await get_weather(message_text=i[4], message_fullname=i[2])
-                await bot.send_message(i[1], current_weather)
+                text = f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} {i[2]} {i[3]} {i[4]}"
+                try:
+                    await bot.send_message(i[1], current_weather)
+                except aiogram.utils.exceptions.BotBlocked:
+                    text += " ЗАБЛОКИРОВАЛ БОТА"
                 with codecs.open("auto_sender_log.txt", 'a', encoding="windows-1251") as file:
-                    text = f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} {i[2]} {i[3]} {i[4]}\n"
-                    file.write(emoji.demojize(text, delimiters=(":", ":")))
+                    file.write(emoji.demojize(text + "\n", delimiters=(":", ":")))
         time_date = datetime.datetime.now()
         time_delta = [60 - time_date.minute, 60 - time_date.second]
         await asyncio.sleep(time_delta[0] * 60 + time_delta[1] - 30)
--- middlewares/check_handler.py
+++ middlewares/check_handler.py
@@ -11,15 +11,17 @@ punctuation = [x for x in punctuation]
 class CheckHandler(BaseMiddleware):
     async def on_process_message(self, message: types.Message, data: dict):
         def dirty_text_checker(text):
+            if text == "/start":
+                return False
+            for i in text:
+                if i in punctuation:
+                    return True
             if emoji.emoji_count(text) > 0:
                 return True
             if len(text) > 30:
                 return True
             if len(text) == 1:
                 return True
-            for i in text:
-                if i in punctuation:
-                    return True
 
         if dirty_text_checker(message.text):
             await message.reply("Вводи корректное название города \U0001F92C")
